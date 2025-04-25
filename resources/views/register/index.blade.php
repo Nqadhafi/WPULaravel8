dfe9191f5263dc51ef750a0b34345b233de3511a
Nqadhafi <qadhafi817@gmail.com>
2025-04-25 15:31:24 +0700

    login/regis form

---
+++ resources/views/register/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+@section('konten')
+<div class="row justify-content-center">
+    <div class="col-lg-6">
+        
+        <main class="form-registration w-100 m-auto">
+            <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
+            <form>
+          
+              <div class="form-floating">
+                <input type="text" name="name" class="form-control rounded-top" id="name" placeholder="Name">
+                <label for="name">Name</label>
+              </div>
+              <div class="form-floating">
+                <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                <label for="username">Username</label>
+              </div>
+              <div class="form-floating">
+                <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+                <label for="email">Email address</label>
+              </div>
+              <div class="form-floating">
+                <input type="password" name="password" class="form-control rounded-bottom" id="password" placeholder="Password">
+                <label for="password">Password</label>
+              </div>
+          
+
+              <button class="btn btn-primary w-100 py-2" type="submit">Register</button>
+              </form>
+              <small class="d-block text-center mt-3">
+                Already Registered? <a href="/login" class="text-decoration-none">Login Now!</a>
+              </small>
+          </main>
+    </div>
+</div>
+@endsection
